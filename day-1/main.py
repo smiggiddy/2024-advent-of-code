58aab91c60b07237a1457c55a72387bd004155bc
Mike Smith <89040888+smiggiddy@users.noreply.github.com>
2024-12-31 17:30:59 -0500

    refactored day1

--- day-1/main.py
+++ day-1/main.py
@@ -1,63 +1,52 @@
-def load_data(file):
-    data = ...
-    with open(file) as f:
-        data = f.readlines()
+from collections import Counter
 
-    data = [line.strip().split() for line in data]
+
+def load_data(file):
+    data = [line.strip().split() for line in open(file)]
     return data
 
 
-def clean_data(data):
-    list_one = []
-    list_two = []
-
-    for _, a in enumerate(data):
-        list_one.append(a[0])
-        list_two.append(a[1])
-
-    return list_one, list_two
-
-
-def compare(a, b):
-    return abs(int(a) - int(b))
-
-
 def solve_one(a, b):
-    sum = 0
-    for a, b in zip(a, b):
-        sum += compare(a, b)
-
-    print(sum)
-    return sum
+    total = sum([abs(int(a) - int(b)) for a, b in zip(a, b)])
+    return total
 
 
 def solve_two(a, b):
+    b_counts = Counter(b)
+    total = sum(int(num) * b_counts[num] for num in a if num in b)
 
-    num_map = {}
-    for count, item in enumerate(a):
-        temp_map = {}
-        for j in b:
-            if j > item:
-                break
-            if j == item:
-                if a[count] in temp_map.keys():
-                    temp_map[item] += 1
-                else:
-                    temp_map[item] = 1
-        num_map = {**num_map, **temp_map}
-
-    similarity = [
-        int(item) * int(num_map[item]) for item in a if item in num_map.keys()
-    ]
-    print(similarity)
-    return sum(similarity)
+    return total
 
 
 # data = load_data("sample_input.txt")
 data = load_data("input_one.txt")
-a, b = clean_data(data)
+a, b = map(list, zip(*(d for d in data)))
 
 a.sort()
 b.sort()
-solve_one(a, b)
-solve_two(a, b)
+print(solve_one(a, b), solve_two(a, b))
+
+# data = ...
+# with open(file) as f:
+#     data = f.readlines()
+
+# sum = 0
+# for a, b in zip(a, b):
+#     sum += abs(int(a) - int(b))
+# num_map = {}
+# for count, item in enumerate(a):
+#     temp_map = {}
+#     for j in b:
+#         if j > item:
+#             break
+#         if j == item:
+#             if a[count] in temp_map.keys():
+#                 temp_map[item] += 1
+#             else:
+#                 temp_map[item] = 1
+#     num_map = {**num_map, **temp_map}
+#
+# similarity = [
+#     int(item) * int(num_map[item]) for item in a if item in num_map.keys()
+# ]
+# total = sum(similarity)
